78a5c2a428ea59cf3bbd7f38bf1bef3966f899ac
于淼 <yufree@yumiaodeMacBook-Pro.local>
2014-09-10 14:26:50 +0800

    add pdf temp

--- maps.R
+++ maps.R
@@ -11,11 +11,12 @@ plot(mar, axes=T,xlim=c(118.6,119),ylim=c(37.1,37.3),add=T)
 
 # China maps
 
-adm$ <- getData('GADM', country='China', level=1)
+adm0 <- getData('GADM', country='China', level=0)
 adm1 <- getData('GADM', country='Taiwan', level=0)
 adm2 <- getData('GADM', country='Hong kong', level=0)
 adm3 <- getData('GADM', country='Macao', level=0)
-plot(adm)
+plot(adm0)
 plot(adm1, add = T)
 plot(adm2, add = T)
 plot(adm3, add = T)
+
