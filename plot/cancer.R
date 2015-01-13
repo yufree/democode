cd87bdd36e86fe51c74a012016e0051881483967
Yufree <yufree@live.cn>
2015-01-13 21:12:59 +0800

    typo

--- plot/cancer.R
+++ plot/cancer.R
@@ -16,12 +16,12 @@ summary(fit3)$r.squared
 png('cancer.png')
 par(mfrow=c(2,2))
 
-plot(log(cancer$Lifetime.cancer.incidence)~log(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('log x and log y ',r^2,' 0.646')),xlab='Total stem cell division',ylab='lifetime risk')
+plot(log(cancer$Lifetime.cancer.incidence)~log(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('log x and log y ',R^2,' 0.646')),xlab='Total stem cell division',ylab='lifetime risk')
 
-plot(cancer$Lifetime.cancer.incidence~cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.,pch=19,main=expression(paste('no transformation ',r^2,' 0.284')),xlab='Total stem cell division',ylab='lifetime risk')
+plot(cancer$Lifetime.cancer.incidence~cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.,pch=19,main=expression(paste('no transformation ',R^2,' 0.284')),xlab='Total stem cell division',ylab='lifetime risk')
 
-plot(cancer$Lifetime.cancer.incidence~log(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('log x ',r^2,' 0.183')),xlab='Total stem cell division',ylab='lifetime risk')
+plot(cancer$Lifetime.cancer.incidence~log(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('log x ',R^2,' 0.183')),xlab='Total stem cell division',ylab='lifetime risk')
 
-plot(cancer$Lifetime.cancer.incidence~sqrt(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('squared x ',r^2,' 0.356')),xlab='Total stem cell division',ylab='lifetime risk')
+plot(cancer$Lifetime.cancer.incidence~sqrt(cancer$Cumulative.number.of.divisions.of.all.stem.cells.per.lifetime..lscd.),pch=19,main=expression(paste('squared x ',R^2,' 0.356')),xlab='Total stem cell division',ylab='lifetime risk')
 dev.off()
 
