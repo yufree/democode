ca89c49810a48bbb65eec55f9015667ddfa71741
Yufree <yufree@live.cn>
2016-10-14 09:20:14 -0400

    add

--- plot/motage.sh
+++ plot/motage.sh
@@ -2,11 +2,11 @@
 mkdir final
 _now=$(date +"%m_%d_%Y_%m_%s")
 i=0
-for f in *.tiff; do
+for f in *.png; do
   convert "$f" miff:-
   ((i++))
 done | montage -       \
    -tile 2x2           \
    -geometry 800x800   \
-   final/huge_$_now.tiff
+   final/huge_$_now.png
  # system('motage.sh')
