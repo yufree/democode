62aebd1ce5fedc2e47ed69366089b8b241c3485b
Yufree <yufree@live.cn>
2017-03-09 15:15:57 -0500

    add

--- meta/RAW.R
+++ meta/RAW.R
@@ -42,3 +42,64 @@ getQCraw <- function(path,mzrange,rtrange,index=NULL){
     }
     return(area)
 }
+
+getbrmd <- function(mass){
+        sf <- 78/77.91051
+        sm <- mass*sf
+        sd <- ceiling(sm)-sm
+        df <- as.data.frame(cbind(mass,sm,sd))
+        plot(df$sd~df$sm,xlab = 'm/z',ylab = 'scaled MD')
+        return(df)
+}
+
+getch2md <- function(mass){
+        sf <- 14.00000/14.01565
+        sm <- mass*sf
+        sd <- ceiling(sm)-sm
+        df <- as.data.frame(cbind(mass,sm,sd))
+        plot(df$sd~df$sm,xlab = 'm/z',ylab = 'scaled MD')
+        return(df)
+}
+
+getmassdefect <- function(mass,sf){
+        sm <- mass*sf
+        sd <- ceiling(sm)-sm
+        df <- as.data.frame(cbind(mass,sm,sd))
+        plot(df$sd~df$sm,xlab = 'm/z',ylab = 'scaled MD')
+        return(df)
+}
+
+findbr <- function(xset,n=10,sf = 78/77.91051,step = 0.001,stepsd=0.003, cutoffint = 100, cutoffr=0.4){
+        data <- as.data.frame(groups(xset))
+        df <- peaks(xset)
+        ind <- groupidx(xset)
+        int <- NULL
+        for(i in 1:length(ind)){
+                int[i] <- mean(df[as.numeric(ind[[i]]),'into'])
+        }
+        data$int <- int
+        data <- data[order(data$mzmed),]
+        data <- data[data$npeaks >= n,]
+        mz <- data$mzmed
+        data$sm <- mz*sf
+        data$sd <- ceiling(data$sm)-data$sm
+        smstep <- seq(0,1,step)
+        plot(data$sd~data$sm,type = 'n',xlab = 'm/z',ylab = 'scaled MD')
+        result <- NULL
+        for(i in 1:length(smstep)){
+                mini = smstep[i]-stepsd
+                maxi = smstep[i]+stepsd
+                index = data$sd<maxi & data$sd>mini
+                li <- data[index&data$int>cutoffint,]
+                if(nrow(li)>2){
+                        ratio <- max(li$int[li$int!=max(li$int)])/max(li$int)
+                        if(ratio>cutoffr){
+                                points(li$sd~li$sm,pch = 19,col = 'red')
+                        }
+                }else{
+                        points(li$sd~li$sm,pch = 19,col = 'red')
+                }
+                result <- as.data.frame(rbind(result,li))
+        }
+        return(result[!duplicated(result), ])
+}
