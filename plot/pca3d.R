91622c8943c837a271c84fff37dad3008e2ba310
Yufree <yufree@live.cn>
2017-06-14 12:04:53 -0400

    update

--- plot/pca3d.R
+++ plot/pca3d.R
@@ -1,8 +1,10 @@
 library(pca3d)
 library(rgl)
 library(readr)
+library(car)
+
 QDA_data_normalized <- read_csv("data/QDA_data_normalized.csv")
-QDA_data_normalized <- read_csv("data/QDA_data_processed.csv")
+# QDA_data_normalized <- read_csv("data/QDA_data_processed.csv")
 
 gr <- factor(t(QDA_data_normalized[,2]))
 pca <- prcomp(QDA_data_normalized[,-c(1,2)],scale. = T,center = T)
@@ -12,15 +14,17 @@ groups <- gr
 group.col <- c(rep("#FCB712",8),rep("#F37022",11),rep("#CD004D",9),rep("#6460AA",6),rep("#0089D1",5),rep("#0CB14B",8))
 group.coln <- c("#FCB712","#F37022","#CD004D","#6460AA","#0089D1","#0CB14B")
 
+# 1
 pca3d(pca, col = group.col,group=groups,show.ellipses = T,axes.color = "black",show.group.labels = T,ellipse.ci = .95,show.plane = F, legend = 'topright')
 
+# 2
 pca3d <- function(pca,legend = F,groups, group.col = c(rep("#FCB712",8),rep("#F37022",11),rep("#CD004D",9),rep("#6460AA",6),rep("#0089D1",5),rep("#0CB14B",8)), group.coln = c("#FCB712","#F37022","#CD004D","#6460AA","#0089D1","#0CB14B")){
         levs <- levels(groups)
         x <- pca$x[,1]
         y <- pca$x[,2]
         z <- pca$x[,3]
         open3d()
-        plot3d(x, y, z,xlab = 'PC 1',ylab = 'PC 2', zlab = 'PC 3', col = group.col,type = 'p')
+        plot3d(x, y, z,xlab = 'PC 1 (23%)',ylab = 'PC 2 (18.3%)', zlab = 'PC 3 (13.7%)', col = group.col,type = 'p')
         for (i in 1:length(levs)) {
                 group <- levs[i]
                 selected <- groups == group
@@ -37,3 +41,6 @@ pca3d <- function(pca,legend = F,groups, group.col = c(rep("#FCB712",8),rep("#F3
 }
 pca3d(pca,groups = gr)
 #rgl.postscript("plot.pdf",fmt="pdf")
+
+# 3
+scatter3d(x=pca$x[,1],y=pca$x[,2],z = pca$x[,3],xlab = 'PC 1',ylab = 'PC 2', zlab = 'PC 3', col = group.col,ellipsoid = T,level = 0.95,surface = F,groups = gr)
