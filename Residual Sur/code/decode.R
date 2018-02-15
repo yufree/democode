4181d19adac7b312505368f8093d12fa0eeb912b
Yufree <yufree@live.cn>
2018-02-15 17:37:59 +0800

    update residual sur and add data

---
+++ Residual Sur/code/decode.R
@@ -0,0 +1,8 @@
+# this codes partly came from Prof. John Staudenmayer's code which is post on Prof. Leonard A. Stefanski's website.
+# fit regression and plot to show that it works
+load('data.RData')
+reg <- lm(X1~ .,data=data)
+plot(reg$fitted,reg$resid,pch=16,
+     main="Residual plot from data",
+     xlab = 'fitted',
+     ylab = 'res')
